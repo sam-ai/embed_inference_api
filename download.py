67ad87014ab74058eb2b79da6ea8b5407e68e961
unknown <sambitsekharai@gmail.com>
2023-12-19 04:44:38 +0530

    path

--- download.py
+++ download.py
@@ -1,4 +1,4 @@
 from huggingface_hub import snapshot_download
 
 
-snapshot_download(repo_id="BAAI/bge-small-en-v1.5", local_dir='./models/BAAI/bge-small-en-v1.5')
+snapshot_download(repo_id="BAAI/bge-small-en-v1.5", local_dir='models/BAAI/bge-small-en-v1.5')
